845ff484fc30cac6a718719984f22b3d5198821e
Abdul Halim <halim@rokomari.com>
2022-11-10 17:47:28 +0600

    Implement generate short url api and update UI

--- frontend/src/components/AddUrl.tsx
+++ frontend/src/components/AddUrl.tsx
@@ -1,36 +1,90 @@
-import { useState, SyntheticEvent } from "react";
+import axios from "axios";
+import { useState, SyntheticEvent, useRef } from "react";
 
 const AddUrl = () => {
   const [url, setUrl] = useState('');
+  const [isValidUrl, setIsValidUrl] = useState(true);
+  const [showShortUrl, setShowShortUrl] = useState(false);
+  const [resData, setResData] = useState({
+    urlId: '',
+    originalUrl: '',
+    shortUrl: '',
+  });
+
+  const btnRef: any = useRef();
+
+  const handleUrlChange = (e: any) => {
+    setUrl(e.target.value);
+    if (/^(ftp|http|https):\/\/[^ "]+$/.test(e.target.value)) {
+      setIsValidUrl(true);
+    } else {
+      setIsValidUrl(false);
+    }
+  }
+
+  const getUrls = async () => await axios.get('/urls/all');
+
+  const generateShortUrl = async () => {
+    const response = await axios.post('/urls/short', { originalUrl: url });
+    const { data }  = response || {};
+    if (Object.keys(data).length > 0) {
+      setResData(data);
+      getUrls();
+      setShowShortUrl(true);
+    } else {
+      setShowShortUrl(false);
+    }
+  }
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    generateShortUrl();
     setUrl('');
-    console.log(url);
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(resData.shortUrl);
+    btnRef.current.textContent = 'Copied';
+    btnRef.current.style.backgroundColor = '#20cf6f';
   }
 
   return (
     <div className="mt-12 mx-auto w-[40%] bg-white rounded-sm shadow">
       <form className="p-8 w-full flex" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="url"
-          id="url"
-          className="px-5 py-3 w-3/4 border border-slate-300 rounded"
-          placeholder="Enter a valid url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
+        <div className="w-3/4">
+          <input
+            type="text"
+            name="url"
+            id="url"
+            className={`px-5 py-3 w-full border border-slate-300 rounded ${!isValidUrl && 'border-red-500 outline-0'}`}
+            placeholder="Enter a valid url"
+            value={url}
+            onChange={handleUrlChange}
+          />
+          {!isValidUrl && <span className="text-sm text-red-500">Please provide a valid url</span>}
+        </div>
         <div className="ml-6 w-1/4">
           <button
             type="submit"
-            className={`px-9 py-[14px] text-sm text-white font-medium bg-teal-500 border rounded hover:bg-teal-400 transition duration-300 ease ${!url && 'cursor-not-allowed bg-teal-400'}`}
-            disabled={!url}
+            className={`px-9 py-[14px] text-sm text-white font-medium bg-teal-500 border rounded hover:bg-teal-400 transition duration-300 ease ${(!url || !isValidUrl) && 'cursor-not-allowed bg-teal-400'}`}
+            disabled={!url || !isValidUrl}
           >
             Submit
           </button>
         </div>
       </form>
+      {showShortUrl && (
+        <div className="py-4 text-center border-t bg-slate-100">{resData.shortUrl}
+          <button
+            type="button"
+            className="ml-4 px-4 py-2 text-sm border rounded text-white bg-slate-600"
+            onClick={copyToClipboard}
+            ref={btnRef}
+          >
+            Copy
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/components/DataTable/Table.tsx
+++ frontend/src/components/DataTable/Table.tsx
@@ -1,6 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+type urlResponse = {
+  _id: string,
+  originalUrl: string,
+  shortUrl: string,
+  clicks: number,
+  createdAt: string
+};
+
 const Table = () => {
+  const [urls, setUrls] = useState([]);
+
+  const getUrls = async () => {
+    const response = await axios.get('/urls/all');
+    const { data } = response;
+    const { isSuccess, total, urls } = data;
+    setUrls(urls);
+    console.log(isSuccess, total);
+  };
+
+  useEffect(() => {
+    getUrls();
+  }, []);
+
   return (
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table className="w-full text-sm text-left text-gray-500 break-all dark:text-gray-400">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
           <th scope="col" className="py-3 px-6">
@@ -21,40 +46,25 @@ const Table = () => {
         </tr>
       </thead>
       <tbody>
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            cssafsa5fd4sa5df4sad5
-          </td>
-          <td className="py-4 px-6">
-            https://www.rokomari.com/book/category/56
-          </td>
-          <td className="py-4 px-6">
-            http://localhost/3SDF5A
-          </td>
-          <td className="py-4 px-6">
-              0
-          </td>
-          <td className="py-4 px-6">
-            17th November, 2022
-          </td>
-        </tr>
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            cssafsa5fd4sa5df4sad5
-          </td>
-          <td className="py-4 px-6">
-            https://www.rokomari.com/book/category/56
-          </td>
-          <td className="py-4 px-6">
-            http://localhost/3SDF5A
-          </td>
-          <td className="py-4 px-6">
-              0
-          </td>
-          <td className="py-4 px-6">
-            17th November, 2022
-          </td>
-        </tr>
+        {urls && urls.length > 0 && urls.map((u: urlResponse) => (
+          <tr key={u._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {u._id}
+            </td>
+            <td className="py-4 px-6">
+              {u.originalUrl}
+            </td>
+            <td className="py-4 px-6">
+              {u.shortUrl}
+            </td>
+            <td className="py-4 px-6">
+              {u.clicks}
+            </td>
+            <td className="py-4 px-6">
+              {u.createdAt}
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
